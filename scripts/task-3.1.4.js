5ec44d669fc0861fd2981283a20dbe94adad991a
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-05 20:52:34 +0000

    Task 3.1.4 created and used arithemetic operators

---
+++ scripts/task-3.1.4.js
@@ -0,0 +1,9 @@
+var numberOfKittens = 5;
+var numberOfPuppies = 4;
+
+var numberOfAnimals = numberOfKittens * numberOfPuppies
+console.log(numberOfAnimals);
+
+alert(numberOfAnimals);
+
+document.write(numberOfAnimals);
