b47d53fad23d0467f65055c397f673bbb7725c90
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-10 21:16:30 +0000

    used else if for code output

---
+++ scripts/task-3.2.5.js
@@ -0,0 +1,16 @@
+var temperature = 31;
+
+if (temperature < 0) {
+ 
+    console.log ('Stay inside');
+
+} else if (temperature < 30 ) {
+ 
+    console.log('Put a coat on and a hat');
+
+} else if (temperature < 50) {
+
+      console.log('Put a coat on');
+
+} else if (temperature > 50) {
+        console.log('Wear anything you want');}
