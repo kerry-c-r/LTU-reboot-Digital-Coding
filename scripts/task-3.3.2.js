b5bcffac286e9c8296d3a5cb5401b3a79c35c29e
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-12 20:35:36 +0000

    JS 3.3.2

---
+++ scripts/task-3.3.2.js
@@ -0,0 +1,22 @@
+function drinkOrder( drink, size) {
+
+var drinkToLowerCase = drink.toLowerCase();
+
+switch (drink) {
+   case 'cola':
+   console.log('You have ordered a ' + size + '' + drinkToLowerCase  );
+   break:
+   
+   case 'lemonade':
+    console.log('You have ordered a ' + size + ''  + drinkToLowerCase  );
+    break:
+
+    case 'orange':
+        console.log('You have ordered a ' + size + ''  + drinkToLowerCase );
+        break:
+
+}
+   default:
+    console.log('Sorry, that drink is not available';
+    break:
+}
