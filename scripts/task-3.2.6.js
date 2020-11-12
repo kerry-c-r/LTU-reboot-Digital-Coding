5d9d48e6865585a14433aa09c43eeba743c772d6
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-12 19:27:52 +0000

    Create task-3.2.6.js

---
+++ scripts/task-3.2.6.js
@@ -0,0 +1,22 @@
+var temperature = 51;
+
+if (temperature < 0) {
+ 
+    console.log ('Stay inside');
+
+} else if (temperature < 30 ) {
+ 
+    console.log('Put a coat on and a hat');
+
+} else if (temperature < 50) {
+
+      console.log('Put a coat on');
+
+} else if { (temperature > 50 && temperature ===55) {
+    console.log('its too hot')
+
+}else if { (tempature > 50 || temperature ===55 )}
+console.log('too hot, going to be burnt')
+
+} else { (temperature > 50) {
+        console.log('Wear anything you want');}
