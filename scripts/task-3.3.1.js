2f80618b7bd3a5d7d91bcf283db981cb7b1e7983
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-12 19:45:21 +0000

    function for 3.3.1

---
+++ scripts/task-3.3.1.js
@@ -0,0 +1,8 @@
+function percentageCalculator ( number, percentage){
+    var percentageOf = ( number * percentage ) / 100;
+// BODMAS 
+
+return percentageOf;
+
+      console.log(percentageOf );
+}
