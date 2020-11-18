97586484c69d916aaa4de345d34df7811103a4fc
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-18 16:10:54 +0000

    Alex's code for task 3.1.6

---
+++ scripts/task-3.1.6.1.js
@@ -0,0 +1,19 @@
+var billTotal = 31.20;
+var tipPercentage = 12.5;
+var tipAmount = (billTotal / 100) * tipPercentage;
+
+function nameOfFunction (){
+     // do something
+}
+
+nameOfFunction ();
+
+console.log (billTotal);
+console.log (tipPercentage);
+console.log (tipAmount);
+
+var totalPaid = billTotal + tipAmount;
+var message = 'Your bill was &pound;' + billTotal + ', you tipped ' + tipPercent + '%, which comes to &pound;' + tipAmount + ', taking your total paid to &pound;' + totalPaid;
+
+console.log(message);
+document.write(message);
