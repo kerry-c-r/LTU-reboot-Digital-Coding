40e8d515d201bbaace069a64076dbb494006a80d
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-19 19:26:26 +0000

    Create task-3.4.3.2.js

---
+++ scripts/task-3.4.3.2.js
@@ -0,0 +1,16 @@
+var objRecipe= [
+    {recipeTitle: 'beans on toast',
+    servings: '1',
+    ingredients: ['baked beans', 'bread', 'butter', 'black pepper'],
+
+
+],
+direction: ['microwave beans in microwave for 3 mins (900w - adjust as per tin instructions)',
+'place bread in toaster',
+'spread butter on toast',
+'put beans onto toast',
+'put blackpepper onto beans'],
+letsCook: function() {
+    console.log("I/'m hungry, le/'s cook" + objRecipe.recipeTitle)
+};
+
