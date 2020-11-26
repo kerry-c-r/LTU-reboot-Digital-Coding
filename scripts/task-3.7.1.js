37723be8bb0938af6f82134eda55a2192a682c15
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-26 21:44:45 +0000

    corrected 3.7.1 code

--- scripts/task-3.7.1.js
+++ scripts/task-3.7.1.js
@@ -1,18 +1,46 @@
 const listItems = document.querySelectorAll ('button.selection');
 
-const allimages = document.querySelectorAll ('.main .image-selctions . images');
+const allimages = document.querySelectorAll ('.main .image-selections .images');
 
 function toggleActiveClass (active) {
 
-    listItems.forEach ( function (item) ){
+    listItems.forEach ( function (item)
+    {
         item.classList.remove ('active');
-    }
-    active.classList.add( 'active');
+    })
+    active.classList.add('active');
 }
 
+
+
+//listItems.forEach( function (item) {
+   // item.addEventListener('click', function () {
+   //     toggleActiveClass (item );
+    //    console.log('You clicked me!');
+   // })
+
+
+function toggleimages (dataClass) {
+    if (dataClass === 'all') {
+        allimages.forEach ( function (image) {
+            image.style.display = 'block';
+        })
+
+    } else {
+        allimages.forEach (image => 
+            { image.dataset.class === dataClass ?
+                 image.style.display ='block' :
+                image.style.display = 'none';
+            })
+
+        }
+    }
+
 listItems.forEach( function (item) {
-    item.addEventListener('click', function ( {
-        toggleActiveClass (item );
-        console.log('You clicked me!');
+
+    item.addEventListener ('click', function () {
+        toggleActiveClass( item);
+        toggleimages(item.dataset.class);
     })
-})
+
+    })
