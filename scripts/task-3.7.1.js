3f8f19dbcd6c8af60dd38760d5938f4ffee336a4
kerry-c-r <72576338+kerry-c-r@users.noreply.github.com>
2020-11-26 20:12:43 +0000

    Create task-3.7.1.js

---
+++ scripts/task-3.7.1.js
@@ -0,0 +1,18 @@
+const listItems = document.querySelectorAll ('button.selection');
+
+const allimages = document.querySelectorAll ('.main .image-selctions . images');
+
+function toggleActiveClass (active) {
+
+    listItems.forEach ( function (item) ){
+        item.classList.remove ('active');
+    }
+    active.classList.add( 'active');
+}
+
+listItems.forEach( function (item) {
+    item.addEventListener('click', function ( {
+        toggleActiveClass (item );
+        console.log('You clicked me!');
+    })
+})
